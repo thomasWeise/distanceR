21b905553314bfc83265eaffd1b1f086fef568ed
Thomas Weise <tweise@hfuu.edu.cn>
2018-04-27 14:07:03 +0800

    First Version with Code Moved from processMineR.modelDist

---
+++ R/create.R
@@ -0,0 +1,20 @@
+#' @title Create an instance of \code{\link[stats]{dist}}
+#' @description Provide a distance matrix and names and then create a get an
+#'   instance of \code{\link[stats]{dist}} in return.
+#' @param distances the distance vector
+#' @param names the vector of names
+#' @return an instance \code{\link[stats]{dist}}
+#' @export dist.create
+#' @importFrom stats dist
+dist.create <- function(distances, names) {
+  distances <- force(distances);
+  names <- force(names);
+  attributes(distances) <- list(Size = length(names),
+                                Labels = names,
+                                Diag = FALSE,
+                                Upper = FALSE,
+                                method = "user");
+  class(distances) <- "dist";
+  distances <- force(distances);
+  return(distances);
+}
